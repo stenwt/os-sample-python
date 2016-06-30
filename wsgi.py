6260a6da673e88b8170349ecbe627012390116ac
Sten Turpin <sten@redhat.com>
2016-06-30 10:13:12 -0500

    first pass at github auth

--- wsgi.py
+++ wsgi.py
@@ -1,11 +1,68 @@
+import os
 from flask import Flask
-application = Flask(__name__)
-
 from rauth.service import OAuth2Service
 
+application = Flask(__name__)
+
+github = OAuth2Service(
+    name='github',
+    base_url='https://api.github.com/',
+    access_token_url='https://github.com/login/oauth/access_token',
+    authorize_url='https://github.com/login/oauth/authorize',
+    client_id= os.environ['GITHUB_CLIENT_ID'],
+    client_secret= os.environ['GITHUB_CLIENT_SECRET'],
+)
+
 @application.route("/")
-def hello():
-    return "Hello World!"
+def index():
+    return render_template('login.html')
+
+@app.route('/about')
+def about():
+    if session.has_key('token'):
+        auth = github.get_session(token = session['token'])
+        resp = auth.get('/user')
+        if resp.status_code == 200:
+            user = resp.json()
+
+        return render_template('about.html', user = user)
+    else:
+        return redirect(url_for('login'))
+
+
+@app.route('/login')
+def login():
+    redirect_uri = url_for('authorized', next=request.args.get('next') or 
+        request.referrer or None, _external=True)
+    print(redirect_uri)
+    # More scopes http://developer.github.com/v3/oauth/#scopes
+    params = {'redirect_uri': redirect_uri, 'scope': 'user:email'} 
+    print(github.get_authorize_url(**params))
+    return redirect(github.get_authorize_url(**params))
+
+@app.route('/github/callback')
+def authorized():
+    # check to make sure the user authorized the request
+    if not 'code' in request.args:
+        flash('You did not authorize the request')
+        return redirect(url_for('index'))
+
+    # make a request for the access token credentials using code
+    redirect_uri = url_for('authorized', _external=True)
+
+    data = dict(code=request.args['code'],
+        redirect_uri=redirect_uri,
+        scope='user:email,public_repo')
+
+    auth = github.get_auth_session(data=data)
+
+    # the "me" response
+    me = auth.get('user').json()
+
+    session['token'] = auth.access_token
+
+    flash('Logged in as ' + me['name'])
+    return redirect(url_for('index'))
 
 if __name__ == "__main__":
     application.run()
